addfc178a849ab36354e3f1362f1639305495ad9
Joey Farruggio <joey@joeyfarruggio.com>
2022-04-23 14:30:10 -0500

    WIP

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,7 @@ require("laravel-mix-tailwind");
 mix.setPublicPath("dist");
 
 mix.css("src/styles.css", "dist/styles.css").tailwind();
-setTimeout(() => {
-	mix.js("src/app.js", "dist/app.js"), 2000;
-});
+mix.js("src/app.js", "dist/app.js");
 
 mix.webpackConfig({
 	module: {
